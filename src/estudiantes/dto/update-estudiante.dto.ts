43fa22a3da13649330019da0fa103aeb9eb4e3b4
carlaarean79 <carlaandreaarean@gmail.com>
2024-04-06 16:41:46 -0300

    crud estudiante completo

--- src/estudiantes/dto/update-estudiante.dto.ts
+++ src/estudiantes/dto/update-estudiante.dto.ts
@@ -1,8 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateEstudianteDto } from './create-estudiante.dto';
+import {EstudianteDto } from './create-estudiante.dto';
 import { IsNumber, IsString } from 'class-validator';
 
-export class UpdateEstudianteDto extends PartialType(CreateEstudianteDto) {
+export class UpdateEstudianteDto extends PartialType(EstudianteDto) {
     @IsNumber()
     idEstudiante: number;
 

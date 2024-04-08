7fc3530603f89dbeb731a4c513dee435bcc6ec52
carlaarean79 <carlaandreaarean@gmail.com>
2024-04-08 10:48:08 -0300

    crud completo dirección estudiante

--- src/direccion-estudiante/direccion-estudiante.controller.ts
+++ src/direccion-estudiante/direccion-estudiante.controller.ts
@@ -1,34 +1,33 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { DireccionEstudianteService } from './direccion-estudiante.service';
-import { CreateDireccionEstudianteDto } from './dto/create-direccion-estudiante.dto';
-import { UpdateDireccionEstudianteDto } from './dto/update-direccion-estudiante.dto';
-
+import { DireccionEstudianteDto } from './dto/create-direccion-estudiante.dto';
+import { DireccionEstudiante } from './entities/direccion-estudiante.entity';
 @Controller('direccion-estudiante')
 export class DireccionEstudianteController {
   constructor(private readonly direccionEstudianteService: DireccionEstudianteService) {}
 
   @Post()
-  create(@Body() createDireccionEstudianteDto: CreateDireccionEstudianteDto) {
-    return this.direccionEstudianteService.create(createDireccionEstudianteDto);
+  createDireccion(@Body() datos: DireccionEstudianteDto): Promise<DireccionEstudiante> {
+    return this.direccionEstudianteService.createDireccion(datos);
   }
 
   @Get()
-  findAll() {
-    return this.direccionEstudianteService.findAll();
+  findDireccionAll() {
+    return this.direccionEstudianteService.findDireccionAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.direccionEstudianteService.findOne(+id);
+  findOneDireccion(@Param('id') id: string) {
+    return this.direccionEstudianteService.findOneDireccion(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDireccionEstudianteDto: UpdateDireccionEstudianteDto) {
-    return this.direccionEstudianteService.update(+id, updateDireccionEstudianteDto);
+  updateDireccion(@Param('id') id: string, @Body() datos: DireccionEstudianteDto) {
+    return this.direccionEstudianteService.updateDireccion(+id, datos);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.direccionEstudianteService.remove(+id);
+  removeDireccion(@Param('id') id: string) {
+    return this.direccionEstudianteService.removeDireccion(+id);
   }
 }
